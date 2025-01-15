4af52262443a9c65039b64567101276fa4bb565d
Omkar Sonawane <163371960+OmkarSonawane1230@users.noreply.github.com>
2025-01-15 15:02:45 +0000

    modified:   pages/registration.html 	modified:   server/controller/controller_t.js 	modified:   server/controller/find.js 	modified:   server/model/model.js 	modified:   server/model/model_t.js

--- server/model/model.js
+++ server/model/model.js
@@ -39,24 +39,6 @@ var schema = new mongoose.Schema({
     },
 })
 
-// const UserData = mongoose.model('userdb', schema);
+const Userdb = mongoose.model('userdb', schema);
 
-// module.exports = Userdb;
-
-
-// const mongoose = require('mongoose');
-
-// var schema = new mongoose.Schema({
-//     username : {
-//         type : String,
-//         required: true,
-//         unique: true
-//     },
-//     password : {
-//         type: String,
-//         required: true,
-//     },
-// })
-
-// const UserDta = mongoose.model('userdb', schema);
-// module.exports = UserData;
+module.exports = Userdb;
--- server/model/model_t.js
+++ server/model/model_t.js
@@ -1,19 +1,19 @@
 
-const UserData = mongoose.model('userdb', schema);
+// const UserData = mongoose.model('userdb', schema);
 
-module.exports = Userdb;
+// module.exports = Userdb;
 
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-var schema = new mongoose.Schema({
-    username : {
-        type : String,
-        required: true,
-        unique: true
-    },
-    password : {
-        type: String,
-        required: true,
-    },
-})
+// var schema = new mongoose.Schema({
+//     username : {
+//         type : String,
+//         required: true,
+//         unique: true
+//     },
+//     password : {
+//         type: String,
+//         required: true,
+//     },
+// })
